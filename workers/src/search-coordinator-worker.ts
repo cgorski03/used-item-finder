9b32b2f37be5f36cdfc947e5524cd84c029cebd3
Colin Gorski <cgorski03@gmail.com>
2025-10-11 16:11:29 -0400

    coordinator worker

--- workers/src/search-coordinator-worker.ts
+++ workers/src/search-coordinator-worker.ts
@@ -11,17 +11,35 @@ export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
         const now = new Date();
         console.log('Request triggered');
-        // All searches that should be published to the queue
-        const searches = db.select().from(search).where(
-            and(
-                eq(search.active, true),
-                or(
-                    // This is a search that has not run yet
-                    isNull(search.nextRun),
-                    lte(search.nextRun, now)
+        try {
+            // All searches that should be published to the queue
+            const searches = await db.select().from(search).where(
+                and(
+                    eq(search.active, true),
+                    or(
+                        // This is a search that has not run yet
+                        isNull(search.nextRun),
+                        lte(search.nextRun, now)
+                    )
                 )
-            )
-        )
-
+            );
+            console.log(`Found ${searches.length}`)
+            const messagesToSend = searches.map(search => ({
+                body: { searchId: search.id },
+            }));
+            if (messagesToSend.length > 0) {
+                // Queue.sendBatch allows sending multiple messages at once
+                await env.search_jobs_queue.sendBatch(messagesToSend);
+                console.log(`Queued ${messagesToSend.length} search jobs to Cloudflare Queue.`);
+            }
+            return new Response(JSON.stringify({
+                success: true,
+                searchesQueued: searches.length,
+            }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+        }
+        catch (error: any) {
+            console.error('coordinator worker has failed');
+            return new Response(JSON.stringify({ error: 'Failed to coordinate searches', details: error.message }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+        }
     }
 }
