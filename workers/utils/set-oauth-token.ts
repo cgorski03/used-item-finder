9cc30ca08a877524fac52c8f5c442dce2052f904
Colin Gorski <cgorski03@gmail.com>
2025-10-12 21:21:23 -0400

    module integration test

--- workers/utils/set-oauth-token.ts
+++ workers/utils/set-oauth-token.ts
@@ -4,18 +4,18 @@ const EXPIRATION_BUFFER_MINUTES = 15;
 
 type AccessTokenKVObject = {
     access_token: string;
-    expiresAt: number; // UTC Timestamp in ms
+    expires_at: number; // UTC Timestamp in ms
 }
 
 export const shouldGetNewToken = (accessToken: unknown, expirationBufferMinutes: number = EXPIRATION_BUFFER_MINUTES): boolean => {
     // type guards
     if (!accessToken || typeof accessToken !== "object") return true;
-    if (!("access_token" in accessToken) || !("expiresAt" in accessToken)) return true;
+    if (!("access_token" in accessToken) || !("expires_at" in accessToken)) return true;
     const tokenObj = accessToken as AccessTokenKVObject;
-    if (typeof tokenObj.expiresAt !== 'number') return true;
+    if (typeof tokenObj.expires_at !== 'number') return true;
     // Check if it is going to expire in the next 15 minutes
     const currentUtcTimestampMs = Date.now();
-    if ((tokenObj.expiresAt - (expirationBufferMinutes * 60 * 1000)) < currentUtcTimestampMs) {
+    if ((tokenObj.expires_at - (expirationBufferMinutes * 60 * 1000)) < currentUtcTimestampMs) {
         return true;
     }
     return false;
@@ -33,7 +33,7 @@ export async function setRefreshToken(item_finder_kv: KVNamespace, EBAY_TOKEN_KE
         const newToken = await getAccessToken()
         const kvObj: AccessTokenKVObject = {
             access_token: newToken.access_token,
-            expiresAt: Date.now() + newToken.expires_in * 1000
+            expires_at: Date.now() + newToken.expires_in * 1000
         }
         // Save new token
         await item_finder_kv.put(EBAY_TOKEN_KEY, JSON.stringify(kvObj))
