292aff288daa2d76afef628dc0956aacdb4b0af1
Colin Gorski <cgorski03@gmail.com>
2025-10-16 20:45:45 -0400

    refactor: ebay interaction, db types

---
+++ cloudflare/workers/search-producer/src/ebay-token-utils.ts
@@ -0,0 +1,38 @@
+import { getAccessToken, AccessTokenKVObject, parseAccessToken, EbayAuthTokenOptions } from "@workers/shared";
+const EXPIRATION_BUFFER_MINUTES = 15;
+
+export const shouldGetNewToken = (accessToken: unknown, expirationBufferMinutes: number = EXPIRATION_BUFFER_MINUTES): boolean => {
+    // type guards
+    const token = parseAccessToken(accessToken);
+    if (!token) return true;
+    // Check if it is going to expire in the next 15 minutes
+    const currentUtcTimestampMs = Date.now();
+    if ((token.expires_at - (expirationBufferMinutes * 60 * 1000)) < currentUtcTimestampMs) {
+        return true;
+    }
+    return false;
+}
+
+export async function setEbayToken(item_finder_kv: KVNamespace, EBAY_TOKEN_KEY: string, ebayCredentials: EbayAuthTokenOptions) {
+    // Get the current token from the KV 
+    let accessToken = await item_finder_kv.get(EBAY_TOKEN_KEY, { type: 'json' });
+    if (!shouldGetNewToken(accessToken)) {
+        console.log(`Token within ${EXPIRATION_BUFFER_MINUTES} minutes. Refreshing token is not necessary`);
+        return;
+    }
+    // Get another token
+    try {
+        const newToken = await getAccessToken(ebayCredentials)
+        const kvObj: AccessTokenKVObject = {
+            access_token: newToken.access_token,
+            expires_at: Date.now() + newToken.expires_in * 1000
+        }
+        // Save new token
+        await item_finder_kv.put(EBAY_TOKEN_KEY, JSON.stringify(kvObj))
+    } catch (error: any) {
+        console.error(`Failed to get new token from ebay API. Error: ${error}`)
+        // rethrow error for observability
+        throw error;
+    }
+}
+
--- cloudflare/workers/search-producer/src/index.ts
+++ cloudflare/workers/search-producer/src/index.ts
@@ -1,14 +1,14 @@
-import { and, eq, isNull, lte, or } from "drizzle-orm";
-import { db, search } from "@db";
-import { setEbayToken } from "@workers/shared";
-
+import { getWorkerDb, search, and, eq, isNull, lte, or } from "@db";
+import { setEbayToken } from "./ebay-token-utils";
+import { EbayAuthTokenOptions } from "@workers/shared";
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+        const db = await getWorkerDb(env.DATABASE_URL);
         const now = new Date();
         console.log('Request triggered');
         try {
             // All searches that should be published to the queue
-            const searches = await (await db).select().from(search).where(
+            const searches = await db.select().from(search).where(
                 and(
                     eq(search.active, true),
                     or(
@@ -24,7 +24,13 @@ export default {
             }));
             if (messagesToSend.length > 0) {
                 // Make sure there is a token 
-                await setEbayToken(env.ITEM_FINDER, env.EBAY_KV_KEY);
+                const ebayOptions: EbayAuthTokenOptions = {
+                    clientId: env.EBAY_CLIENT_ID,
+                    clientSecret: env.EBAY_CLIENT_SECRET,
+                    devid: env.EBAY_DEV_ID,
+                    env: env.EBAY_ENV
+                };
+                await setEbayToken(env.ITEM_FINDER, env.EBAY_KV_KEY, ebayOptions);
                 // Queue.sendBatch allows sending multiple messages at once
                 await env.search_jobs_queue.sendBatch(messagesToSend);
                 console.log(`Queued ${messagesToSend.length} search jobs to Cloudflare Queue.`);
