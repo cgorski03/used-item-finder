e9aafabe98a22ac66df9cad2edf182a2d2f201fe
Colin Gorski <cgorski03@gmail.com>
2025-10-21 20:27:08 -0400

    refactor: removing pointless all item screen

---
+++ web/src/app/(authenticated)/items/saved/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import React from 'react'
+import { trpc } from '@/trpc/react'
+import { ItemList } from '@/trpc/shared';
+import { ItemCard } from '@/components/item/item-card';
+
+export default function SavedItemsPage() {
+    const { data: item, isLoading, isError, error } = trpc.item.getAll.useQuery();
+    const items: ItemList | undefined = trpc.item.getAll.useQuery().data;
+
+    return (
+        <div className="w-full h-full p-4"> {/* Added padding for better aesthetics */}
+            <h1 className="text-2xl font-bold mb-6">Saved Items</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {items && items.map((item: ItemList[number]) => (
+                    <ItemCard key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    );
+}
